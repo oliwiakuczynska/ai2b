ef6d3ebf498f1f5806bf6a7742c6b0c5fa1deeba
Oliwia Kuczyńska <ko49317>
2023-11-23 11:43:08 +0100

    lab a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HomeComponent} from "./home/home.component";
 
 import { AppComponent } from './app.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeComponent,
+    RouterModule,
   ],
-  providers: [],
+  providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
